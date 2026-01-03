8eb87a2db87a65f1c7a11c2d59ee48fa58d6c3a9
NHTanvir <36070006+NHTanvir@users.noreply.github.com>
2026-01-03 15:05:00 +0600

    wpcs

--- includes/classes/Discord_Integration.php
+++ includes/classes/Discord_Integration.php
@@ -580,16 +580,13 @@ class Discord_Integration {
 	/**
 	 * Setup Discord integration settings
 	 */
-	public function setup_discord_settings() {
-		// This would normally be added to a settings page
-		// For now, we're just providing the methods to handle it
-	}
+	public function setup_discord_settings() {}
 
 	/**
 	 * Set up default Discord configuration
 	 */
 	public function setup_default_discord_config() {
-		// Only set defaults if options don't already exist
+
 		if ( empty( get_option( 'gamerz_discord_webhook_url', '' ) ) ) {
 			update_option( 'gamerz_discord_webhook_url', 'https://discord.com/api/webhooks/1443615355024179221/u5J7zMlnZL8HuK5Ux57_bz9GkjWuTWJ5oso3wuzRdPZlEJL3lktLw3rM4y_SdGfiw_3Z' );
 		}
@@ -601,69 +598,68 @@ class Discord_Integration {
 		if ( empty( get_option( 'gamerz_discord_guild_id', '' ) ) ) {
 			update_option( 'gamerz_discord_guild_id', '1045083738754789386' );
 		}
-		
-		// Only set role mappings if they don't already exist
+
 		if ( false === get_option( 'gamerz_discord_role_mapping', false ) ) {
 			$role_mappings = [
-				'Scrubling' => [
-					'role_id' => '',
+				'Scrubling'   => [
+					'role_id'   => '',
 					'role_name' => 'Scrubling'
 				],
 				'Scrub Recruit' => [
-					'role_id' => '1443626808929943595',  // Rookie Scrub
-					'role_name' => 'Rookie Scrub'
+					'role_id'     => '1443626808929943595',  // Rookie Scrub
+					'role_name'   => 'Rookie Scrub'
 				],
-				'Scrub Scout' => [
-					'role_id' => '',
+				'Scrub Scout'  => [
+					'role_id'    => '',
 					'role_name' => 'Scrub Scout'
 				],
 				'Scrub Soldier' => [
-					'role_id' => '1443626957228216340',  // Casual Scrub
-					'role_name' => 'Casual Scrub'
+					'role_id'     => '1443626957228216340',  // Casual Scrub
+					'role_name'   => 'Casual Scrub'
 				],
 				'Scrub Strategist' => [
-					'role_id' => '',
-					'role_name' => 'Scrub Strategist'
+					'role_id'       => '',
+					'role_name'     => 'Scrub Strategist'
 				],
 				'Scrub Captain' => [
-					'role_id' => '',
-					'role_name' => 'Scrub Captain'
+					'role_id'    => '',
+					'role_name'  => 'Scrub Captain'
 				],
 				'Scrub Champion' => [
-					'role_id' => '1443627075520167957',  // Elite Scrub
-					'role_name' => 'Elite Scrub'
+					'role_id'      => '1443627075520167957',  // Elite Scrub
+					'role_name'    => 'Elite Scrub'
 				],
 				'Guild Officer' => [
-					'role_id' => '',
-					'role_name' => 'Guild Officer'
+					'role_id'     => '',
+					'role_name'   => 'Guild Officer'
 				],
 				'Scrub Sage' => [
-					'role_id' => '',
+					'role_id'   => '',
 					'role_name' => 'Scrub Sage'
 				],
 				'Scrub Warlord' => [
-					'role_id' => '',
-					'role_name' => 'Scrub Warlord'
+					'role_id'     => '',
+					'role_name'   => 'Scrub Warlord'
 				],
 				'Meme Master' => [
-					'role_id' => '',
+					'role_id'   => '',
 					'role_name' => 'Meme Master'
 				],
 				'Scrub Overlord' => [
-					'role_id' => '1443627282588762275',  // Mythic Scrub
-					'role_name' => 'Mythic Scrub'
+					'role_id'       => '1443627282588762275',  // Mythic Scrub
+					'role_name'     => 'Mythic Scrub'
 				],
 				'Nova Scrub' => [
-					'role_id' => '',
+					'role_id'   => '',
 					'role_name' => 'Nova Scrub'
 				],
 				'Scrub Prime' => [
-					'role_id' => '',
+					'role_id'   => '',
 					'role_name' => 'Scrub Prime'
 				],
 				'Legendary Scrub' => [
-					'role_id' => '1443627161578766461',  // Legendary Scrub
-					'role_name' => 'Legendary Scrub'
+					'role_id'       => '1443627161578766461',  // Legendary Scrub
+					'role_name'    => 'Legendary Scrub'
 				],
 			];
 			
